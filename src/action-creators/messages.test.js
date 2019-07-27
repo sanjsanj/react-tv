dd7bb3b62a1d513be240e6d1945b2969f8113897
Sanjay Purswani <sanjsanj@hotmail.com>
2019-07-27 09:56:37 +0100

    Refactor out mock messages for tests to one place

--- src/action-creators/messages.test.js
+++ src/action-creators/messages.test.js
@@ -1,22 +1,18 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-jest.mock('../data');
-
 import { getMessages } from '../data';
 import { loadMessages } from './messages';
 import { MESSAGES_LOADED } from './action-types';
+import { messagesPayload } from '../utils/test-helpers';
+
+jest.mock('../data');
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('loadMessages', () => {
-  const mockMessage = {
-    message: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.',
-    userId: 'fe27b760-a915-475c-80bb-7cdf14cc6ef3',
-    id: 'cd445e6d-e514-424f-ba8f-16ec842002c6',
-    timestamp: '2017-02-09T04:27:38Z',
-  };
+  const mockMessage = messagesPayload[0];
 
   beforeAll(() => {
     getMessages.mockImplementation(() => Promise.resolve(mockMessage));
